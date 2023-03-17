20d417ad3a6ba28a422ad4f1a929ce9dc37d6cb1
Robert <rgs2@icomp.ufam.edu.br>
2023-03-16 23:27:49 -0400

    feat: initial config project

---
+++ app/src/main/java/com/robert/project/agenda/ui/activity/ListaDeAlunosActivity.java
@@ -0,0 +1,17 @@
+package com.robert.project.agenda.ui.activity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.robert.project.agenda.R;
+
+public class ListaDeAlunosActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+    }
+}
