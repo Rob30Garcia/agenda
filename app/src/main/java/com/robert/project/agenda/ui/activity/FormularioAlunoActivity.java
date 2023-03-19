c547771f6fbe2c415e7a73df17f49a9e52bb24f6
Robert <rgs2@icomp.ufam.edu.br>
2023-03-19 13:39:01 -0400

    chore: put fields to alunos

---
+++ app/src/main/java/com/robert/project/agenda/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,16 @@
+package com.robert.project.agenda.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.robert.project.agenda.R;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
